94baf883fbd33aefd149ffba7a2a6bf3243a431a
Ben Hollar <benh524@gmail.com>
2023-07-26 20:26:49 -0400

    Changing project structure a bit

---
+++ src/app/_components/index.ts
@@ -0,0 +1,6 @@
+export { RecipeListComponent } from './recipe-list/recipe-list.component';
+export { HomeComponent } from './home/home.component';
+export { FoodListComponent } from './food-list/food-list.component';
+export { PantryComponent } from './pantry/pantry.component';
+export { MealPlanComponent } from './meal-plan/meal-plan.component';
+export { MainNavigationComponent } from './main-navigation/main-navigation.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { HomeComponent } from './home/home.component';
-import { FoodListComponent } from './food-list/food-list.component';
-import { PantryComponent } from './pantry/pantry.component';
-import { MealPlanComponent } from './meal-plan/meal-plan.component';
-import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 import { RouterModule } from '@angular/router';
+import { 
+  RecipeListComponent, HomeComponent, FoodListComponent, PantryComponent, MealPlanComponent, MainNavigationComponent
+} from './_components';
 
 @NgModule({
   declarations: [
